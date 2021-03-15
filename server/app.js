bd4c99849db6cf7221f985c13fb16c31aaeb7bf5
Ilkay Citak <citakilkay@gmail.com>
2021-03-15 15:31:11 +0200

    talep edilen urlye göre serverın döndürdüğü cevap

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const http = require('http');
 const hostName = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer( (req, res) => {
+/*const server = http.createServer( (req, res) => {
     console.log(req.url);
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
@@ -13,7 +13,24 @@ const server = http.createServer( (req, res) => {
 server.listen(port, hostName, () => {
     console.log(`Server şurada Çalışıyor: http://${hostName}:${port}/`);
 })
+*/
 
 // res.setHeader(name, value);
 // server.listen(port, hostname, backlog, callback);
 
+const server = http.createServer( (req, res) =>{
+    if (req.url === '/') {
+        return res.end('HOME PAGE')
+    } else if (req.url === '/about') {
+        return res.end('ABOUT PAGE')
+    } else if (req.url === '/contact') {
+        return res.end('CONTACT PAGE')
+    } else {
+        res.statusCode = 404;
+        res.end('SAYFA BULUNAMADI');
+    }
+})
+
+server.listen(port, hostName, () => {
+    console.log(`Server şurada Çalışıyor: http://${hostName}:${port}/`);
+})
